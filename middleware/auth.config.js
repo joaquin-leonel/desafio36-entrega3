c6d475cf6244ee54b195f22f58d5a889ee102286
joaquin leonel <woriltomo10@gmail.com>
2022-05-24 15:08:38 -0300

    tercera entrega final proyecto Backend

---
+++ middleware/auth.config.js
@@ -0,0 +1,19 @@
+const passport = require('passport')
+const userModel = require('../dao/models/users')
+
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+    
+    passport.deserializeUser((userId, done) => {
+        userModel.findById(userId)
+            .then((user) => {
+                done(null, user);
+            })
+            .catch(err => done(err))
+    });
+
+    // Passport Strategies 
+    require('./auth-passport-local')()
+}
